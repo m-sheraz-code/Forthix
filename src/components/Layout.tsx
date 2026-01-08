bcb4f66df0f44ce3327c710052cc4e51e72e5b3b
Muhammad Sheraz <m.sheraz.work@gmail.com>
2026-01-08 14:05:47 +0500

    7th Commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === '/';
+
   return (
     <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
       <Header />
-      <main>
+      <main className={!isLandingPage ? 'pt-20' : ''}>
         <Outlet />
       </main>
       <Footer />
